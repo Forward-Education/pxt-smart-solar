7400b1d0d7443fe7a53f0db9fb46bc4f7339246b
ssande-fwd <ssande@forwardedu.com>
2025-08-18 11:07:48 -0400

    test namespaces removed

--- tests/ms-solartracking/modify-final.ts
+++ tests/ms-solartracking/modify-final.ts
@@ -1,35 +1,45 @@
-namespace msSolartrackingModify {
-    input.onButtonPressed(Button.A, function () {
-        fwdMotors.setAngle(fwdBase.leftServo, 0)
-        power = 0
-    })
-    function calculatePower() {
-        current_Amps = fwdSensors.current1.current() / 1000
-        power = current_Amps * fwdSensors.voltage1.voltage()
-    }
-    
-    let power = 0
-    let current_Amps = 0
-    current_Amps = 0
-    power = 0
-  
+input.onButtonPressed(Button.A, function () {
     fwdMotors.setAngle(fwdBase.leftServo, 0)
-    basic.forever(function () {
-        calculatePower()
-        if (power > 0.2) {
-            basic.showIcon(IconNames.Diamond)
-            music.play(music.builtinPlayableSoundEffect(soundExpression.twinkle), music.PlaybackMode.UntilDone)
-        } else if (power <= 0.2 && power > 0.001) {
-            basic.showIcon(IconNames.SmallDiamond)
-            music.play(music.tonePlayable(175, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
-            fwdMotors.setAngle(fwdBase.leftServo, fwdMotors.getAngle(fwdBase.leftServo) + 15)
-            if (fwdMotors.getAngle(fwdBase.leftServo) > 170) {
-                fwdMotors.setAngle(fwdBase.leftServo, 0)
-            }
-        } else {
-            fwdMotors.setAngle(fwdBase.leftServo, 0)
-            music.play(music.builtinPlayableSoundEffect(soundExpression.yawn), music.PlaybackMode.UntilDone)
-            basic.showIcon(IconNames.No)
-        }
-    })
+    power = 0
+})
+function calculatePower() {
+    current_Amps = fwdSensors.current1.current() / 1000
+    power = current_Amps * fwdSensors.voltage1.voltage()
 }
+
+let power = 0
+let current_Amps = 0
+current_Amps = 0
+power = 0
+
+fwdMotors.setAngle(fwdBase.leftServo, 0)
+basic.forever(function () {
+    calculatePower()
+    if (power > 0.2) {
+        basic.showIcon(IconNames.Diamond)
+        music.play(
+            music.builtinPlayableSoundEffect(soundExpression.twinkle),
+            music.PlaybackMode.UntilDone
+        )
+    } else if (power <= 0.2 && power > 0.001) {
+        basic.showIcon(IconNames.SmallDiamond)
+        music.play(
+            music.tonePlayable(175, music.beat(BeatFraction.Whole)),
+            music.PlaybackMode.UntilDone
+        )
+        fwdMotors.setAngle(
+            fwdBase.leftServo,
+            fwdMotors.getAngle(fwdBase.leftServo) + 15
+        )
+        if (fwdMotors.getAngle(fwdBase.leftServo) > 170) {
+            fwdMotors.setAngle(fwdBase.leftServo, 0)
+        }
+    } else {
+        fwdMotors.setAngle(fwdBase.leftServo, 0)
+        music.play(
+            music.builtinPlayableSoundEffect(soundExpression.yawn),
+            music.PlaybackMode.UntilDone
+        )
+        basic.showIcon(IconNames.No)
+    }
+})
--- tests/proj-current-monitor/project-final.ts
+++ tests/proj-current-monitor/project-final.ts
@@ -1,8 +1,10 @@
-namespace projCurrentMonitor {
-    // The code within this loop will run every 500 milliseconds (aka 0.5 seconds).
+// The code within this loop will run every 500 milliseconds (aka 0.5 seconds).
 loops.everyInterval(500, function () {
     // This is a conditional statement that evaluates the value of the 'current' running through the Energy Sensor to the load.
-    if (fwdSensors.current1.current() > 5 && fwdSensors.current1.current() <= 20) {
+    if (
+        fwdSensors.current1.current() > 5 &&
+        fwdSensors.current1.current() <= 20
+    ) {
         // If the current is greater than 5 milliamps and less than or equal to 20 milliamps, a single dot will appear on the LEDs. This indicates a low current.
         basic.showLeds(`
             . . . . .
@@ -11,7 +13,10 @@ loops.everyInterval(500, function () {
             . . . . .
             . . # . .
             `)
-    } else if (fwdSensors.current1.current() > 20 && fwdSensors.current1.current() <= 40) {
+    } else if (
+        fwdSensors.current1.current() > 20 &&
+        fwdSensors.current1.current() <= 40
+    ) {
         // If the current is greater than 20 milliamps and less than or equal to 40 milliamps, 9 dots will appear on the LEDs. This indicates a slightly higher current.
         basic.showLeds(`
             . . . . .
@@ -20,7 +25,10 @@ loops.everyInterval(500, function () {
             . # # # .
             # # # # #
             `)
-    } else if (fwdSensors.current1.current() > 40 && fwdSensors.current1.current() <= 60) {
+    } else if (
+        fwdSensors.current1.current() > 40 &&
+        fwdSensors.current1.current() <= 60
+    ) {
         // If the current is greater than 40 milliamps and less than or equal to 60 milliamps, 14 dots will appear on the LEDs. This indicates an even higher current.
         basic.showLeds(`
             . . . . .
@@ -29,7 +37,10 @@ loops.everyInterval(500, function () {
             # # # # #
             # # # # #
             `)
-    } else if (fwdSensors.current1.current() > 60 && fwdSensors.current1.current() <= 80) {
+    } else if (
+        fwdSensors.current1.current() > 60 &&
+        fwdSensors.current1.current() <= 80
+    ) {
         // If the current is greater than 60 milliamps and less than or equal to 80 milliamps, 19 dots will appear on the LEDs. This indicates an even higher current.
         basic.showLeds(`
             . . # . .
@@ -52,5 +63,3 @@ loops.everyInterval(500, function () {
         basic.showIcon(IconNames.No)
     }
 })
-
-}
